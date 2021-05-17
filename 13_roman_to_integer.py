c21cab4d29e6d1e1be04ca60f157890afb1fab61
Eduard <user@Users-MacBook-Pro-2.local>
2021-05-17 17:46:45 +0300

    new

--- 13_roman_to_integer.py
+++ 13_roman_to_integer.py
@@ -1,100 +1,3 @@
-# class Solution:
-#     def romanToInt(self, s: str) -> int:
-#         number_dict = {'I': 1,
-#                        'V': 5,
-#                        'X': 10,
-#                        'L': 50,
-#                        'C': 100,
-#                        'D': 500,
-#                        'M': 1000,
-#                        'IV': 4,
-#                        'IX': 9,
-#                        'XL': 50,
-#                        'XC': 90,
-#                        'CD': 400,
-#                        'CM': 900
-#                        }
-#
-#         test_arr = []
-#         list_s = []
-#         i = 0
-#
-#         print("v")
-#         while i != len(s):
-#             print("d")
-#             if i + 1 != len(s):
-#                 # print(i)
-#                 if (s[i] == 'I' and s[i + 1] == 'V'):
-#                     test_arr.append('IV')
-#                     list_s.append(number_dict['IV'])
-#                     i = i + 2
-#                     continue
-#
-#                 if (s[i] == 'I' and s[i + 1] == 'X'):
-#                     test_arr.append('IX')
-#                     list_s.append(number_dict['IX'])
-#                     i = i + 2
-#                     continue
-#
-#                 if (s[i] == 'X' and s[i + 1] == 'L'):
-#                     test_arr.append('XL')
-#                     list_s.append('XL')
-#                     i = i + 2
-#                     continue
-#
-#                 if (s[i] == 'X' and s[i + 1] == 'C'):
-#                     test_arr.append('XC')
-#                     list_s.append(number_dict['XC'])
-#                     i = i + 2
-#                     continue
-#
-#                 if (s[i] == 'C' and s[i + 1] == 'D'):
-#                     test_arr.append('CD')
-#                     list_s.append(number_dict['CD'])
-#                     i = i + 2
-#                     continue
-#
-#                 if (s[i] == 'C' and s[i + 1] == 'M'):
-#                     test_arr.append('CM')
-#                     list_s.append(number_dict['CM'])
-#                     i = i + 2
-#                     continue
-#
-#                 test_arr.append(s[i])
-#                 list_s.append(number_dict[s[i]])
-#                 i = i + 1
-#             test_arr.append(s[i])
-#             list_s.append(number_dict[s[i]])
-#
-#         # print(test_arr)
-#         # print(list_s)
-#         # print(sum(list_s))
-#         return sum(list_s)
-#
-#
-# cl1 = Solution()
-#
-#
-# cl1.romanToInt("III")
-#
-
-
-# class Solution:
-#     def romanToInt(self, s):
-#         """
-#         :type s: str
-#         :rtype: int
-#         """
-#         a = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-#         ans=0
-#         for i in range(len(s)):
-#             if i<len(s)-1 and a[s[i]]<a[s[i+1]]:
-#                 ans-=a[s[i]]
-#             else:
-#                 ans+=a[s[i]]
-#         return ans
-
-
 class Solution:
     def romanToInt(self, s):
         ans = 0
@@ -121,12 +24,43 @@ class Solution:
         return ans
 
 
-sl = Solution()
-print(sl.romanToInt("IV"))
-print(sl.romanToInt("III"))
-print(sl.romanToInt("IX"))
-print(sl.romanToInt("LVIII"))
-print(sl.romanToInt("MCMXCIV"))
+# s1 = Solution()
+# print(s1.romanToInt("IV"))
+# print(s1.romanToInt("III"))
+# print(s1.romanToInt("IX"))
+# print(s1.romanToInt("LVIII"))
+# print(s1.romanToInt("MCMXCIV"))
+
+
+class Solution2:
+    def romanToInt(self, s):
+        ans = 0
+
+        a = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+             'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
+
+        lenght = len(s)
+        i=0
+        while i<=lenght-1:
+            tmp = s[i:i+2]
+            if s[i:i+2]  in a:
+                ans = ans + a[s[i:i+2]]
+                i = i + 2
+            else:
+                tmp = s[i]
+                ans = ans + a[s[i]]
+                i+=1
+
+        return ans
+
+
+s2 = Solution2()
+print(s2.romanToInt("IV"))
+print(s2.romanToInt("III"))
+print(s2.romanToInt("IX"))
+print(s2.romanToInt("LVIII"))
+print(s2.romanToInt("MCMXCIV"))
+
 
 
 
